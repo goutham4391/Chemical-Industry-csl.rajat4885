029d4f086dc93657220da23089a895e60dac8d61
rajat4885 <100342306+rajat4885@users.noreply.github.com>
2022-10-06 00:32:23 +0530

    initial

---
+++ app/common/scripts/functional.js
@@ -0,0 +1,25 @@
+﻿//csl plasma ------------------
+function togglePreferredCenter(nearestId) {
+   if (!nearestId) {
+      return;
+   }
+
+   jQuery(function ($) {
+      //set icon based on nearest center id
+      var centerEl = $(".js-prefercenter[data-itemid='" + nearestId + "']");
+      if (centerEl.length > 0) {
+         var isActive = centerEl.hasClass("active");
+
+         //clear existing
+         $(".js-prefercenter").each(function () { $(this).removeClass("active"); });
+
+         if (isActive) {
+            deleteCookie("nearestcenter");
+         }
+         else {
+            setCookie("nearestcenter", nearestId, 14);
+            centerEl.addClass("active");
+         }
+      }
+   });
+}
